74e64017a02b792136a7787fc02011f40873a9f4
jose luis <cortezluisjose@gmail.com>
2020-05-13 01:43:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,16 +1,34 @@
 require_relative 'config/environment'
 
-class App < Sinatra:Base
-  
-  get '/hello/"name' do
-    
-    
+class App < Sinatra::Base
+
+  # This is a sample static route.
+  get '/hello' do
+    "Hello World!"
+  end
+
+  # This is a sample dynamic route.
+  get "/hello/:name" do
+    @user_name = params[:name]
+    "Hello #{@user_name}!"
   end
   
-  get '/medicines/:id' do
-  @medicine = all_the_medicines.select do |medicine|
-    medicine.id == params[:id]
-  end.first
-  erb :'/medicines/show.html'
-end
+  get "/goodbye/:name" do 
+ @name = params[:name]
+    "Goodbye, #{@name}."
+  end
+
+  get "/:operation/:number1/:number2" do
+    @number_1 = params[:number1].to_i
+    @number_2 = params[:number2].to_i
+    if params[:operation] == "add"
+      "#{@number_1+@number_2}"
+    elsif params[:operation] == "subtract"
+      "#{@number_1-@number_2}"
+    elsif params[:operation] == "multiply"
+      "#{@number_1*@number_2}"
+    elsif params[:operation] == "divide"
+      "#{@number_1/@number_2}"
+    end
+  end
 end
